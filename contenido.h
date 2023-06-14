b892400383fd04ba1767747d272a4e17d96bab07
Cesar Flores <A01751101@tec.mx>
2023-06-14 12:52:58 -0600

    .

--- contenido.h
+++ contenido.h
@@ -1,5 +1,5 @@
-#ifndef CONTENIDO_H
-#define CONTENIDO_H
+#ifndef contenido_H
+#define contenido_H
 
 #include <iostream>
 #include <string>
@@ -8,17 +8,10 @@ using std::string;
 class contenido{
     public:
         virtual void allVideos() = 0;
-<<<<<<< HEAD
         virtual void showVidsCalif(float calif) = 0;
         virtual void showVidsGen(string gen) = 0;
         virtual void showEp(string nomS) = 0;
         virtual void showPeli(float calif) = 0;
-=======
-        virtual void showVideosCalif() = 0;
-        virtual void showVidsGen(string gen)  = 0;
-        virtual void showEp(string nomS) = 0;
-        virtual void showPeli (float calif) = 0;
->>>>>>> 5218962d8cc6d0984097fd1f3c543484d9c9f066
         virtual void calificar(string vidNom, float calif) = 0;
 };
 
--- video.h
+++ video.h
@@ -1,50 +1,22 @@
-<<<<<<< HEAD
-#ifndef VIDEOS_H
-#define VIDEOS_H
-=======
-#ifndef VIDEO_H
-#define VIDEO_H
->>>>>>> 5218962d8cc6d0984097fd1f3c543484d9c9f066
+#ifndef video_H
+#define video_H
 
 #include <iostream>
 #include <string>
 
 using std::string;
 
-<<<<<<< HEAD
 class video{
-=======
-class Video{
->>>>>>> 5218962d8cc6d0984097fd1f3c543484d9c9f066
     private:
         string idV;
         string nombre;
         string duracion;
         string fecha;
         float calificacion;
-<<<<<<< HEAD
     public:
-        video(string idV1,string nombre1,string duracion1, string fecha1, float calificacion1){
-            idV = idV1;
-            nombre = nombre1;
-            duracion = duracion1;
-            fecha = fecha1;
-            calificacion = calificacion1;
-        }
+        video(string idV1,string nombre1,string duracion1, string fecha1, float calificacion1);
 
-        void disp(){
-            
-        };
-=======
-        int numEp;
-    public:
-        Video();
-        Video(string idV, string nombre, string duracion, string fecha, float calificacion, int numEp);
-        Video(string idV, string nombre, string duracion, string fecha, float calificacion);
-        string getName();
-        void display();
-        
->>>>>>> 5218962d8cc6d0984097fd1f3c543484d9c9f066
+        void disp();
 };
 
 #endif
